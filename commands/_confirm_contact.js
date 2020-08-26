d021e9ebe6b50a1e7dfde1194c95cfe32c928c57
barklgn <65805233+barklgn@users.noreply.github.com>
2020-08-26 20:03:28 +0300

    Update _confirm_contact.js

--- commands/_confirm_contact.js
+++ commands/_confirm_contact.js
@@ -22,7 +22,7 @@ let ls=User.getProperty("last_name")
 let na=User.getProperty("name")
 let tex = "👤Name: "+na+" : "+ls+"\n ❇️address: "+ad+"\n 📞phone: "+ph
 if(data.message=="/yes"){
-   Bot.sendMessage("💯እናመሰግናለን መረጃዎ ተመዝግቧል\n•*ይህንን ግሩፕ ይቀላቀሉ https://t.me/joinchat/FOrdbk2KeugHNgV_b0JC3A ")
+   Bot.sendMessage("💯እናመሰግናለን መረጃዎ ተመዝግቧል\n•*ይህንን ግሩፕ ይቀላቀሉ* https://t.me/joinchat/FOrdbk2KeugHNgV_b0JC3A ")
  Api.sendMessage({ chat_id: 350936430, text: tex , parse_mode: "html" })
 }else{
   Bot.sendMessage("Change data /change");
